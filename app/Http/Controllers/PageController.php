cc02e4f9f522bae62257cf0768fd720dc0c42c37
= <=khofifahmanda04@gmail.com>
2022-03-16 05:58:40 +0700

    first commit

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index()
+    {
+        return view('index');
+    }
+
+    public function about()
+    {
+        return view('about');
+    }
+
+    public function testmonial()
+    {
+        return view('testmonial');
+    }
+
+    public function clients()
+    {
+        return view('clients');
+    }
+
+    public function contact()
+    {
+        return view('contact');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\PageController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('index');
+// });
+Route::get('/', [PageController::class, 'index']);
+
+Route::get('/about', [PageController::class, 'about']);
+
+Route::get('/testmonial', [PageController::class, 'testmonial']);
+
+Route::get('/clients', [PageController::class, 'clients']);
+
+Route::get('/contact', [PageController::class, 'contact']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
